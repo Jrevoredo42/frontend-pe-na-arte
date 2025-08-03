88be3433910e8b6fa5cee5ff17b8a5f9e6db2d8b
Jrevoredo42 <revoredojf@gmail.com>
2025-08-02 23:53:23 -0300

    (style): configuração geral das estilizações globais de cores, fontes e relacionados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 
 import "./globals.css";
 import { Inter, Poppins } from "next/font/google";
+import HomeNav from "@/Components/nav";
+
 
 // Fontes Google
 const inter = Inter({
@@ -30,7 +32,19 @@ export default function RootLayout({
         <title>PE na Arte</title>
       </head>
       <body>
-        {children}
+        <HomeNav/>
+      <div
+        className="fixed inset-0 w-full h-full -z-10"
+        aria-hidden="true"
+        style={{
+          backgroundImage: "url('/pattern cordas.svg')",
+          backgroundRepeat: "repeat",
+          backgroundSize: "cover"
+        }}
+        >
+          <div className="absolute inset-0 bg-gray-100/80"></div>
+        </div>
+      {children}
       </body>
     </html>
   );
